e76c4a1ff049ed52a4c60c66e0f586d8ed299991
Syed Hussain Ather <shussainather@gmail.com>
2023-06-09 14:43:32 -0400

    removed unnecessary lines

--- programv2.py
+++ programv2.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import requests
 import requests_cache
 
@@ -7,13 +9,22 @@ requests_cache.install_cache(cache_name='conference_cache', backend='sqlite', ex
 
 class ConferenceScheduler:
     def __init__(self):
+        """
+        Initialize the Conference Scheduler with the list of conferences.
+        """
         self.conferences = []
+        self.api_url = 'https://backendassessmentv1.onrender.com/conference'
+        self.solution_url = 'https://backendassessmentv1.onrender.com/solution'
 
     def conference_api_call(self):
         response = requests.get(API_ENDPOINT)
-        data = response.json()
-        self.create_schedule(data)
-
+        if response.status_code == 200:
+            partners = response.json()['partners']
+            print("Response was successful")
+            return response.json
+        else:
+            print("Response failed. Status code", request.status_code)
+        
     def create_schedule(self, data):
         partners = data['partners']
         countries = set([partner['country'] for partner in partners])
@@ -28,7 +39,12 @@ class ConferenceScheduler:
                 date1 = sorted_partners[0]['availableDates'][i]
                 date2 = sorted_partners[0]['availableDates'][i + 1]
                 
-                if date2 - date1 == 86400:  # 86400 seconds = 1 day
+                # Convert date strings to datetime objects
+                date1 = datetime.strptime(date1, "%Y-%m-%d")
+                date2 = datetime.strptime(date2, "%Y-%m-%d")
+                time_diff = (date2 - date1).total_seconds() # Get the difference between the two times.
+
+                if time_diff == 86400:  # 86400 seconds = 1 day
                     start_date = date1
                     break
             
@@ -57,6 +73,9 @@ class ConferenceScheduler:
 
 class Conference:
     def __init__(self, country, start_date, attendee_count, attendees):
+        """
+        Initialize the Conference class with country, start_date, attendee_count, and attendees.  
+        """
         self.name = country
         self.startDate = start_date
         self.attendeeCount = attendee_count
@@ -66,4 +85,6 @@ class Conference:
 scheduler = ConferenceScheduler()
 
 # Call the conference_api_call method to fetch data and create the schedule
-scheduler.conference_api_call()
+data = scheduler.conference_api_call()
+schedule = scheduler.create_schedule(data)
+scheduler.submit_schedule(schedule)
