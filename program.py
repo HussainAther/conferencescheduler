7b8ec0e230a44d56662eaa127e05f19a9df1ae25
Syed Hussain Ather <shussainather@gmail.com>
2023-06-09 15:06:36 -0400

    error exception handling

--- program.py
+++ program.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 import json
 import requests
 import requests_cache
@@ -15,78 +15,77 @@ class DateTimeEncoder(json.JSONEncoder):
 
 class ConferenceScheduler:
     def __init__(self):
-        """
-        List of conferences.
-        """
         self.conferences = []
 
     def conference_api_call(self):
-        """
-        Perform the API call for the conference.
-        """
-        response = requests.get(API_ENDPOINT)
-        data = response.json()
-        self.create_schedule(data)
+        try:
+            response = requests.get(API_ENDPOINT)
+            response.raise_for_status()  # Raise exception for non-200 status codes
+            data = response.json()
+            self.create_schedule(data)
+        except requests.exceptions.RequestException as e:
+            print("Error occurred during API call:", str(e))
+            # Handle the error gracefully or raise the exception to the caller
 
-    def create_schedule(self, data): 
-        """
-        Create the schedule itself.
-        """
-        partners = data['partners'] # Get the list of partners.
-        countries = set([partner['country'] for partner in partners])
+    def create_schedule(self, data):
+        try:
+            partners = data['partners']
+            countries = set([partner['country'] for partner in partners])
 
-        for country in countries:
-            country_partners = [partner for partner in partners if partner['country'] == country] # Find the partners by country. 
-            sorted_partners = sorted(country_partners, key=lambda x: len(x['availableDates']), reverse=True) # Get the available dates for each parter pair.
-            attendees = sorted_partners[0]['email'] # Get the email addresses for each attendee.
-            start_date = None
+            for country in countries:
+                country_partners = [partner for partner in partners if partner['country'] == country]
+                sorted_partners = sorted(country_partners, key=lambda x: len(x['availableDates']), reverse=True)
+                attendees = sorted_partners[0]['email']
+                start_date = None
 
-            for i in range(len(sorted_partners[0]['availableDates']) - 1):
-                date1 = sorted_partners[0]['availableDates'][i]
-                date2 = sorted_partners[0]['availableDates'][i + 1]
+                for i in range(len(sorted_partners[0]['availableDates']) - 1):
+                    date1 = sorted_partners[0]['availableDates'][i]
+                    date2 = sorted_partners[0]['availableDates'][i + 1]
 
-                # Convert date strings to datetime objects
-                date1 = datetime.strptime(date1, "%Y-%m-%d")
-                date2 = datetime.strptime(date2, "%Y-%m-%d")
-                time_diff = (date2 - date1).total_seconds()  # Get the difference between the two times.
+                    date1 = datetime.strptime(date1, "%Y-%m-%d")
+                    date2 = datetime.strptime(date2, "%Y-%m-%d")
+                    time_diff = (date2 - date1).total_seconds()
 
-                if time_diff == 86400: # 86400 seconds = 1 day
-                    start_date = date1
-                    break
+                    if time_diff == 86400:
+                        start_date = date1
+                        break
 
-            conference = Conference(country, start_date, len(sorted_partners), attendees)
-            self.conferences.append(conference)
+                conference = Conference(country, start_date, len(sorted_partners), attendees)
+                self.conferences.append(conference)
 
-        self.post_schedule()
+            self.post_schedule()
+        except (KeyError, ValueError) as e:
+            print("Error occurred while creating the schedule:", str(e))
+            # Handle the error gracefully or raise the exception to the caller
 
     def post_schedule(self):
-        """
-        Post the schedule. 
-        """
-        schedule = {'Conferences': []} # Initialize a schedule variable.
+        try:
+            schedule = {'Conferences': []}
 
-        for conference in self.conferences: # Search through the conferences in the list of conferences.
-            conference_data = {
-                'name': conference.name,
-                'startDate': conference.startDate,
-                'attendeeCount': conference.attendeeCount,
-                'attendees': [conference.attendees]
-            }
-            schedule['Conferences'].append(conference_data)
+            for conference in self.conferences:
+                conference_data = {
+                    'name': conference.name,
+                    'startDate': conference.startDate,
+                    'attendeeCount': conference.attendeeCount,
+                    'attendees': [conference.attendees]
+                }
+                schedule['Conferences'].append(conference_data)
 
-        json_data = json.dumps(schedule, cls=DateTimeEncoder)  # Convert the schedule to JSON
+            json_data = json.dumps(schedule, cls=DateTimeEncoder)
 
-        response = requests.get(API_ENDPOINT, json=json_data)
-        if response.status_code == 200:
-            print("Schedule posted successfully.")
-        else:
-            print("Failed to post schedule.")
+            response = requests.post(API_ENDPOINT, json=json_data)
+            response.raise_for_status()  # Raise exception for non-200 status codes
+
+            if response.status_code == 200:
+                print("Schedule posted successfully.")
+            else:
+                print("Failed to post schedule.")
+        except requests.exceptions.RequestException as e:
+            print("Error occurred while posting the schedule:", str(e))
+            # Handle the error gracefully or raise the exception to the caller
 
 class Conference:
     def __init__(self, country, start_date, attendee_count, attendees):
-        """
-        Initialize the Conference class with the country, start_date, attendee_count, and attendees.
-        """
         self.name = country
         self.startDate = start_date
         self.attendeeCount = attendee_count
